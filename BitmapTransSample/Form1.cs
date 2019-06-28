8a5648d7af191e909b0605170fa604961497052f
Hideaki Konishi <hhd00655@nifty.ne.jp>
2019-06-28 13:42:41 +0900

    init

---
+++ BitmapTransSample/Form1.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing.Drawing2D;
+using System.Timers;
+
+namespace BitmapTransSample
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        int x = 0;
+        int y = 0;
+
+        Bitmap bitmap1;
+        Bitmap bitmap2;
+
+        protected override void OnPaint(PaintEventArgs e)
+        {
+          //  Bitmap bmp = new Bitmap("ball.bmp");
+
+            Graphics FormG = this.CreateGraphics();
+            bitmap2.SetResolution(FormG.DpiX, FormG.DpiY);
+
+            for (int x = 0; x < 5; x++)
+            {
+                for (int y = 0; y < 5; y++)
+                {
+                    e.Graphics.DrawImage(bitmap1, x * 100, y * 100);
+                }
+            }
+
+            e.Graphics.DrawImage(bitmap2, x, 0);
+            e.Graphics.DrawImage(bitmap2, x * 1.2f,100);
+            e.Graphics.DrawImage(bitmap2, x * 1.5f, 200);
+
+            e.Graphics.DrawImage(bitmap2, 100, y * 1.2f);
+            e.Graphics.DrawImage(bitmap2, 150, y * 1.7f);
+            e.Graphics.DrawImage(bitmap2, 200, y * 2.0f);
+
+            e.Graphics.DrawImage(bitmap2, x * 1.2f, y * 1.2f);
+            e.Graphics.DrawImage(bitmap2, x * 1.7f, y * 1.7f);
+            e.Graphics.DrawImage(bitmap2, x * 1.7f, y * 2.0f);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+           
+            bitmap1 = new Bitmap(Properties.Resources.pie1);
+            bitmap2 = new Bitmap(Properties.Resources.pie2);
+
+            // タイマーの生成
+            var timer = new System.Timers.Timer();
+            timer.Elapsed += new ElapsedEventHandler(OnElapsed_TimersTimer);
+            timer.Interval = 20;
+
+            // タイマーを開始
+            timer.Start();
+        }
+
+        private void OnElapsed_TimersTimer(object sender, ElapsedEventArgs e)
+        {
+            if (x > 300)
+            {
+                x = 0;
+            }
+            if (y > 300)
+            {
+                y = 0;
+            }
+            x += 10;
+            y += 10;
+
+            Invalidate();
+        }
+    }
+}
