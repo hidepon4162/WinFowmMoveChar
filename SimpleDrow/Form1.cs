a113853a46d6c7a9f5754d4a82316b2eea9c477c
hidepon4162 <hidepon4162_imac@yahoo.co.jp>
2023-02-25 07:37:38 +0900

    たいまー移動処理をForm1に移動

--- SimpleDrow/Form1.cs
+++ SimpleDrow/Form1.cs
@@ -1,4 +1,5 @@
-﻿using System.Drawing;
+﻿using System;
+using System.Drawing;
 using System.Windows.Forms;
 
 namespace SimpleDrow
@@ -16,6 +17,29 @@ namespace SimpleDrow
 
             Point position = new Point(0, 0);
             pie = new Pie(this, position);
+
+            // タイマーの生成
+            Timer timer = new Timer();
+            timer.Interval = 100;
+            timer.Tick += Update;
+
+            // タイマーを開始
+            timer.Start();
+        }
+
+        private void Update(object sender, EventArgs e)
+        {
+            int x = pie.Position.X;
+            x += 10;
+            pie.Position = new Point(x, pie.Position.Y);
+
+            if (pie.Position.X > 300)
+            {
+                x = 0;
+                pie.Position = new Point(x, pie.Position.Y);
+            }
+
+            Invalidate();
         }
 
         protected override void OnPaint(PaintEventArgs e)
--- SimpleDrow/Pie.cs
+++ SimpleDrow/Pie.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Drawing;
+﻿using System.Drawing;
 using System.Windows.Forms;
 
 namespace SimpleDrow
@@ -8,47 +7,23 @@ namespace SimpleDrow
     {
         Form form;
 
-        Bitmap pieBitmap;
-
         public Point Position { get; set; }
+        public Bitmap PieBitmap { get; }
 
         public Pie(Form form, Point position)
         {
             this.form = form;
             Position = position;
 
-            pieBitmap = new Bitmap(Properties.Resources.pie2);
-
-            // タイマーの生成
-            Timer timer = new Timer();
-            timer.Interval = 100;
-            timer.Tick += Update;
-
-            // タイマーを開始
-            timer.Start();
-        }
-
-        private void Update(object sender, EventArgs e)
-        {
-            int x = Position.X;
-            x += 10;
-            Position = new Point(x, Position.Y);
-
-            if (Position.X > 300)
-            {
-                x = 0;
-                Position = new Point(x, Position.Y);
-            }
-
-            form.Invalidate();
+            PieBitmap = new Bitmap(Properties.Resources.pie2);
         }
 
         public void OnPaint(PaintEventArgs e)
         {
             Graphics formGraph = form.CreateGraphics();
-            pieBitmap.SetResolution(formGraph.DpiX, formGraph.DpiY);
+            PieBitmap.SetResolution(formGraph.DpiX, formGraph.DpiY);
 
-            e.Graphics.DrawImage(pieBitmap, Position);
+            e.Graphics.DrawImage(PieBitmap, Position);
         }
     }
 }
